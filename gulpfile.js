f561b0274d3158a067ead6a592c782b027767f8e
Richard Kotze <rich_taylormade@hotmail.com>
2014-12-03 23:29:54 +0000

    init free scroll

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp'),
+rename = require('gulp-rename'),
+uglify = require('gulp-uglify');
+
+var DEST = 'build/';
+
+gulp.task('default', function() {
+  return gulp.src('src/free-scroll.js')
+    // This will output the non-minified version
+    .pipe(gulp.dest(DEST))
+    // This will minify and rename to foo.min.js
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST));
+});
---
+++ src/free-scroll.js
@@ -0,0 +1,68 @@
+
+(function (window) {
+  var document = window.document,
+    // helper methods
+    push = [].push,
+    slice = [].slice,
+    splice = [].splice,
+    forEach = [].forEach,
+    eventsCache = {};
+
+  function FreeScroll(selector, callback) {
+
+    if (!(this instanceof FreeScroll)) {
+      return new FreeScroll(selector);
+    }
+
+    if (!selector) {
+      return this;
+    }
+
+    if (selector instanceof FreeScroll) {
+      return selector;
+    }
+
+    if (selector.nodeType) {
+      this[0] = selector;
+      this.length = 1;
+      return this;
+    }
+
+    if (typeof selector === 'string') {
+      return push.apply(this, slice.call(document.querySelectorAll(selector)));
+    }
+
+    if (typeof selector === 'function') {
+      return FreeScroll(document).ready(selector);
+    }
+  };
+
+
+  FreeScroll.prototype = {
+    length: 0,
+
+    toInfinity : function(){
+
+    },
+
+    on: function(event){
+
+    },
+
+    off: function(event){
+
+    },
+
+    _fire: function(){
+
+    }
+
+  };
+
+  FreeScroll.fn = FreeScroll.prototype;
+
+  FreeScroll.prototype.splice = splice;
+
+  // expose to global object
+  window.FreeScroll = window.FS = FreeScroll;
+}(window));
