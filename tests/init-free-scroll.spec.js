66fef338a382c932a4c9b3290439eb4cb46f893d
Richard Kotze <rich_taylormade@hotmail.com>
2015-01-27 23:18:59 +0000

    move option map tests for two different instances into mapping spec.

--- tests/init-free-scroll.spec.js
+++ tests/init-free-scroll.spec.js
@@ -21,31 +21,4 @@ describe('Initalise free scroll', function(){
 		expect(FS._noMore).toBeDefined();
 	});
 
-	describe('set options for two new free scroll items', function(){
-
-		it('the options should be different', function(){
-			var fs1 = FS({
-				selector:'#none', 
-				distance: 70, 
-				requestData: {
-					urlFormat: '/api/listing?page_number={0}&page_size={1}',
-					pageNumber: 1
-				} 
-			});
-			var fs2 = FS({
-				selector:'#some', 
-				distance: 120, 
-				requestData: {
-					urlFormat: '/api/articles?page_number={0}&page_size={1}',
-					pageNumber: 2
-				} 
-			});
-
-			expect(fs1).not.toEqual(fs2);
-			expect(fs1.options.requestData.pageNumber).not.toBe(fs2.options.requestData.pageNumber);
-			expect(fs1.options.requestData.pageNumber).toBe(1);
-			expect(fs2.options.requestData.pageNumber).toBe(2);
-		});
-	})
-
 });
--- tests/option-mapping.spec.js
+++ tests/option-mapping.spec.js
@@ -72,4 +72,31 @@ describe('Option mapping', function(){
 	    expect(actual).toEqual(mergedOptions);
 	});
 
+	describe('set options for two new free scroll items', function(){
+
+		it('the options should be different', function(){
+			var fs1 = FS({
+				selector:'#none', 
+				distance: 70, 
+				requestData: {
+					urlFormat: '/api/listing?page_number={0}&page_size={1}',
+					pageNumber: 1
+				} 
+			});
+			var fs2 = FS({
+				selector:'#some', 
+				distance: 120, 
+				requestData: {
+					urlFormat: '/api/articles?page_number={0}&page_size={1}',
+					pageNumber: 2
+				} 
+			});
+
+			expect(fs1).not.toEqual(fs2);
+			expect(fs1.options.requestData.pageNumber).not.toBe(fs2.options.requestData.pageNumber);
+			expect(fs1.options.requestData.pageNumber).toBe(1);
+			expect(fs2.options.requestData.pageNumber).toBe(2);
+		});
+	});
+
 });
