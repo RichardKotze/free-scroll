939bc133a71c2151aa6b2a04e816a2a34f9a8c87
Richard Kotze <rich_taylormade@hotmail.com>
2015-04-07 23:51:28 +0100

    updated public api tests.

--- tests/init-free-scroll.spec.js
+++ tests/init-free-scroll.spec.js
@@ -4,16 +4,24 @@ describe('Initalise free scroll', function(){
 		expect(FreeScroll).toBeDefined();
 	});
 
-	it('has a on method', function(){
-		expect(FreeScroll.fn.on).toBeDefined();
+	it('has a more method', function(){
+		expect(FreeScroll.fn.more).toBeDefined();
 	});
-
-	it('has a off method', function(){
-		expect(FreeScroll.fn.off).toBeDefined();
+	
+	it('has a finish method', function(){
+		expect(FreeScroll.fn.finish).toBeDefined();
 	});
-
-	it('has a protected fire method', function(){
-		expect(FreeScroll.fire).toBeDefined();
+	
+	it('has a addEvent method', function(){
+		expect(FreeScroll.fn.addEvent).toBeDefined();
+	});
+	
+	it('has a removeEvent method', function(){
+		expect(FreeScroll.fn.removeEvent).toBeDefined();
+	});
+	
+	it('has a each method', function(){
+		expect(FreeScroll.fn.each).toBeDefined();
 	});
 
 	it('has a protected noMore method', function(){
